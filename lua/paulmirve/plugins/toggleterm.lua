e1642c7c372855ccb7f55016bbd0390a22f14164
Paul Miranda Vega <paul_mv3@hotmail.com>
2022-11-28 17:39:53 -0800

    added global toggleterminal global functions and floating terminal

--- lua/paulmirve/plugins/toggleterm.lua
+++ lua/paulmirve/plugins/toggleterm.lua
@@ -5,4 +5,15 @@ end
 
 toggleterm.setup({
 	open_mapping = [[<c-/>]],
+	direction = "float",
+	float_opts = {
+		border = "curved",
+	},
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local node = Terminal:new({ cmd = "node", hidden = true })
+
+function _NODE_TOGGLE()
+	node:toggle()
+end
